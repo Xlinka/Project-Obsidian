8c55b68692be4c7e035f24ce447791ed28f2c876
xLinka <Oliver.dunnington@outlook.com>
2023-10-01 22:40:16 +0100

    i hate github

--- ProtoFlux/Math/Physics/CentripetalForceCalculationNode.cs
+++ ProtoFlux/Math/Physics/CentripetalForceCalculationNode.cs
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-﻿using Elements.Core;
+using Elements.Core;
 using FrooxEngine;
 using ProtoFlux.Core;
 using ProtoFlux.Runtimes.Execution;
 using ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Physics;
 using System;
-=======
-﻿using System;
-using Elements.Core;
-using ProtoFlux.Core;
->>>>>>> 1f3689c5903a431368bec8a85948d2884a1ffc17
 
 public class CentripetalForceCalculationNode : ValueFunctionNode<ExecutionContext, float>
 {
@@ -19,49 +13,20 @@ public class CentripetalForceCalculationNode : ValueFunctionNode<ExecutionContex
 
     protected override float Compute(ExecutionContext context)
     {
-        float result = 0f;
-
         try
         {
-<<<<<<< HEAD
             float m = Mass.Evaluate(context);
             float v = Velocity.Evaluate(context);
             float r = Radius.Evaluate(context);
 
-            result = (m * v * v) / r;
-=======
-            try
-            {
-                var m = Mass.ReadValue(context);
-                var v = Velocity.ReadValue(context);
-                var r = Radius.ReadValue(context);
+            float result = (m * v * v) / r;
 
-                // Debugging statements
-                UniLog.Log($"Mass: {m}");
-                UniLog.Log($"Velocity: {v}");
-                UniLog.Log($"Radius: {r}");
-
-                var result = (m * v * v) / r;
-
-                // Debugging the result
-                UniLog.Log($"Result: {result}");
-
-                return result;
-            }
-            catch (Exception ex)
-            {
-                // Log any exceptions
-                UniLog.Log($"Error in CentripetalForceCalculationNode.Compute: {ex.Message}");
-                throw;
-            }
->>>>>>> 1f3689c5903a431368bec8a85948d2884a1ffc17
+            return result;
         }
         catch (Exception ex)
         {
-            // Log any exceptions without throwing them
-            UniLog.Log($"Error in CentripetalForceCalculationNode.Compute: {ex.Message}");
+            // Log any exceptions if needed
+            throw new Exception($"Error in CentripetalForceCalculationNode.Compute: {ex.Message}");
         }
-
-        return result;
     }
 }
--- ProtoFlux/Math/Physics/DragCalculationNode.cs
+++ ProtoFlux/Math/Physics/DragCalculationNode.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using Elements.Core;
+﻿using Elements.Core;
+using FrooxEngine;
 using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using System;
 
 namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Physics
 {
@@ -16,21 +18,14 @@ namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Physics
         {
             try
             {
-<<<<<<< HEAD
                 float rho = FluidDensity.Evaluate(context);
                 float3 v = ObjectVelocity.Evaluate(context);
                 float Cd = DragCoefficient.Evaluate(context);
                 float A = CrossSectionalArea.Evaluate(context);
-=======
-                var rho = FluidDensity.ReadValue(context);
-                var v = ObjectVelocity.ReadValue(context);
-                var Cd = DragCoefficient.ReadValue(context);
-                var A = CrossSectionalArea.ReadValue(context);
->>>>>>> 1f3689c5903a431368bec8a85948d2884a1ffc17
 
-                var vMagnitudeSquared = MathX.Dot(v, v);
-                var dragDirection = Normalize(v);
-                var dragForce = -0.5f * rho * vMagnitudeSquared * Cd * A * dragDirection;
+                float vMagnitudeSquared = MathX.Dot(v, v);
+                float3 dragDirection = Normalize(v);
+                float3 dragForce = -0.5f * rho * vMagnitudeSquared * Cd * A * dragDirection;
 
                 return dragForce;
             }
@@ -42,7 +37,6 @@ namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Physics
 
             return float3.Zero;
         }
-<<<<<<< HEAD
 
         // Define Normalize function since MathX doesn't have one
         private float3 Normalize(float3 vector)
@@ -51,15 +45,6 @@ namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Physics
             if (magnitude == 0)
                 return float3.Zero;  // Handle zero magnitude case
             return new float3(vector.x / magnitude, vector.y / magnitude, vector.z / magnitude);
-=======
-        
-        //this was done since Mathx doesnt have a normalise function why 
-        private static float3 Normalize(float3 vector)
-        {
-            var magnitude = MathX.Sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
-            return magnitude == 0 ? new float3(0) : // Handle zero magnitude case
-                new float3(vector.x / magnitude, vector.y / magnitude, vector.z / magnitude);
->>>>>>> 1f3689c5903a431368bec8a85948d2884a1ffc17
         }
     }
 }
