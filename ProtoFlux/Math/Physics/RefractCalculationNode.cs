c8f620d2911d2443d678cbaf3ebe69eb6fd5b50e
xLinka <Oliver.dunnington@outlook.com>
2023-10-02 11:21:33 +0100

    RefractNode

---
+++ ProtoFlux/Math/Physics/RefractCalculationNode.cs
@@ -0,0 +1,31 @@
+using FrooxEngine;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using System;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Physics
+{
+    [NodeCategory("Obsidian/Math/Physics")]
+    public class RefractionNode : ValueFunctionNode<ExecutionContext, float>
+    {
+        public ValueInput<float> RefractiveIndex1;  // Refractive index of medium 1
+        public ValueInput<float> RefractiveIndex2;  // Refractive index of medium 2
+        public ValueInput<float> AngleOfIncidence;  // Angle of incidence in degrees
+
+        protected override float Compute(ExecutionContext context)
+        {
+            float n1 = RefractiveIndex1.Evaluate(context);
+            float n2 = RefractiveIndex2.Evaluate(context);
+            float theta1Rad = AngleOfIncidence.Evaluate(context) * (float)Math.PI / 180.0f;  // Convert angle to radians 
+
+            // Calculate using Snell's Law
+            float sinTheta2 = n1 * (float)Math.Sin(theta1Rad) / n2;
+
+            // Ensure value is within [-1, 1] due to numerical inaccuracies
+            sinTheta2 = Math.Min(Math.Max(sinTheta2, -1.0f), 1.0f);
+
+            float theta2Rad = (float)Math.Asin(sinTheta2);
+            return theta2Rad * 180.0f / (float)Math.PI;  // Convert angle back to degrees
+        }
+    }
+}
