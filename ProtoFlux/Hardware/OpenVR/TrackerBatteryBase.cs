107a15a25805a02ff9e3d10d9baedb7936680d53
xLinka <Oliver.dunnington@outlook.com>
2023-12-16 16:06:54 +0000

    Tracker

---
+++ ProtoFlux/Hardware/OpenVR/TrackerBatteryBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project-Obsidian.ProtoFlux.Hardware.OpenVR
+{
+    internal class FileName
+{
+}
+}
