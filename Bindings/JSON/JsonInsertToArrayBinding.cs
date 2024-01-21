ef5fd216f4257d8c1808c90005af1d0d20c857a8
xLinka <Oliver.dunnington@outlook.com>
2024-01-21 23:26:35 +0000

    JSON Nodes

---
+++ Bindings/JSON/JsonInsertToArrayBinding.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Elements.Core;
+using FrooxEngine;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Json;
+using Newtonsoft.Json.Linq;
+using FrooxEngine.ProtoFlux;
+
+
+    [Category(new string[] { "ProtoFlux/Runtimes/Execution/Nodes/Obsidian/Json" })]
+    public class JsonInsertToArrayBinding<T> : FrooxEngine.ProtoFlux.Runtimes.Execution.ObjectFunctionNode<ExecutionContext, JArray>
+    {
+        public readonly SyncRef<INodeObjectOutput<JArray>> Array;
+        public readonly SyncRef<INodeObjectOutput<T>> Object;
+        public readonly SyncRef<INodeObjectOutput<int>> Index;
+
+        public override Type NodeType => typeof(JsonInsertToArrayNode<T>);
+
+        public JsonInsertToArrayNode<T> TypedNodeInstance { get; private set; }
+
+        public override INode NodeInstance => TypedNodeInstance;
+
+        public override int NodeInputCount => base.NodeInputCount + 3;
+
+        public override N Instantiate<N>()
+        {
+            if (TypedNodeInstance != null)
+            {
+                throw new InvalidOperationException("Node has already been instantiated");
+            }
+            JsonInsertToArrayNode<T> jsonInsertToArrayInstance = (TypedNodeInstance = new JsonInsertToArrayNode<T>());
+            return jsonInsertToArrayInstance as N;
+        }
+
+        protected override void AssociateInstanceInternal(INode node)
+        {
+            if (node is JsonInsertToArrayNode<T> typedNodeInstance)
+            {
+                TypedNodeInstance = typedNodeInstance;
+                return;
+            }
+            throw new ArgumentException("Node instance is not of type " + typeof(JsonInsertToArrayNode<T>));
+        }
+
+        public override void ClearInstance()
+        {
+            TypedNodeInstance = null;
+        }
+
+        protected override ISyncRef GetInputInternal(ref int index)
+        {
+            ISyncRef inputInternal = base.GetInputInternal(ref index);
+            if (inputInternal != null)
+            {
+                return inputInternal;
+            }
+            switch (index)
+            {
+                case 0:
+                    return Array;
+                case 1:
+                    return Object;
+                case 2:
+                    return Index;
+                default:
+                    index -= 3;
+                    return null;
+            }
+        }
+    }
---
+++ ProtoFlux/JSON/JsonInsertToArrayNode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using Elements.Core;
+using FrooxEngine;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Json
+{
+    [NodeCategory("ProtoFlux/Obsidian/Json")]
+    [GenericTypes(typeof(byte), typeof(sbyte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long),
+                  typeof(ulong), typeof(float), typeof(double), typeof(string), typeof(Uri), typeof(JToken), typeof(JObject),
+                  typeof(JArray))]
+    public class JsonInsertToArrayNode<T> : ObjectFunctionNode<ExecutionContext, JArray>
+    {
+        public readonly ObjectInput<JArray> Array;
+        public readonly ObjectInput<T> Object;
+        public readonly ObjectInput<int> Index;
+
+        protected override JArray Compute(ExecutionContext context)
+        {
+            var array = Array.Evaluate(context);
+            var obj = Object.Evaluate(context);
+            var index = Index.Evaluate(context);
+            if (array == null || obj == null || index < 0 || index > array.Count)
+                return null;
+
+            try
+            {
+                var output = (JArray)array.DeepClone();
+                JToken token = obj is JToken jToken ? jToken : new JValue(obj);
+                output.Insert(index, token);
+                return output;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
