6beac3541ea6a88ec655c40a199661706c78af03
xLinka <Oliver.dunnington@outlook.com>
2024-01-21 21:23:53 +0000

    ALL THE STRINGS

---
+++ ProtoFlux/NodeExstensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elements.Core;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using FrooxEngine.UIX;
+
+namespace Obsidian;
+public static class NodeExtensions
+{
+    public static readonly Dictionary<char, string> CharToMorse = new()
+    {
+        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."}, {'E', "."},
+        {'F', "..-."}, {'G', "--."}, {'H', "...."}, {'I', ".."}, {'J', ".---"},
+        {'K', "-.-"}, {'L', ".-.."}, {'M', "--"}, {'N', "-."}, {'O', "---"},
+        {'P', ".--."}, {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+        {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"}, {'Y', "-.--"},
+        {'Z', "--.."}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"},
+        {'5', "....."}, {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."},
+        {'0', "-----"}
+    };
+
+    public static readonly Dictionary<string, char> MorseToChar = CharToMorse.ToDictionary(i => i.Value, j => j.Key);
+}
---
+++ ProtoFlux/Strings/EncodeBase64Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using FrooxEngine;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Strings
+{
+    [NodeCategory("ProtoFlux/Obsidian/String")]
+    public class EncodeBase64Node : ObjectFunctionNode<ExecutionContext, string>
+    {
+        public readonly ObjectInput<string> Input;
+
+        protected override string Compute(ExecutionContext context)
+        {
+            var input = Input.Evaluate(context);
+            return string.IsNullOrEmpty(input) ? null : Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
+        }
+    }
+}
