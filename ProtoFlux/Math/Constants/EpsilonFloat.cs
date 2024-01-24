e87d95381ef651bfde47fdaf09f0681ceb5f13ba
xLinka <Oliver.dunnington@outlook.com>
2024-01-24 00:44:51 +0000

    some nodes

---
+++ ProtoFlux/Math/Constants/EpsilonFloat.cs
@@ -0,0 +1,13 @@
+﻿using Elements.Core;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+[NodeCategory("Protoflux/Obsidian/Math/Constants")]
+[NodeName("Epsilon Float")]
+public class EpsilonFloat : ValueFunctionNode<ExecutionContext, float>
+{
+    protected override float Compute(ExecutionContext context)
+    {
+        return MathX.FLOAT_EPSILON;
+    }
+}
