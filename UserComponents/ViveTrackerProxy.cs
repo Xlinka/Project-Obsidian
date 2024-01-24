daa59c9fbb89739ce2de46be87805f901d04658a
art0007i <art0007i@gmail.com>
2024-01-24 14:10:52 +0100

    add vive battery node

---
+++ UserComponents/ViveTrackerProxy.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using FrooxEngine;
+
+namespace Obsidian;
+
+public class ViveTrackerProxy : UserComponent
+{
+    public readonly Sync<BodyNode> TrackerBodyNode;
+
+    public readonly Sync<bool> IsTrackerActive;
+
+    public readonly SyncRef<ValueStream<float>> BatteryLevel;
+
+    public readonly SyncRef<ValueStream<bool>> BatteryCharging;
+
+    private ViveTracker _currentTracker;
+
+    protected override void OnCommonUpdate()
+    {
+        if (base.User.IsLocalUser)
+        {
+            ViveTracker device = InputInterface.GetDevices<ViveTracker>().FirstOrDefault(t => t.CorrespondingBodyNode == TrackerBodyNode.Value);
+            if (device != _currentTracker)
+            {
+                TrackerBodyNode.Value = device?.CorrespondingBodyNode ?? BodyNode.NONE;
+                BatteryLevel.Target = device?.BatteryLevel.GetStream(base.World);
+                BatteryCharging.Target = device?.BatteryCharging.GetStream(base.World);
+                IsTrackerActive.Value = device != null;
+                _currentTracker = device;
+            }
+        }
+    }
+
+    protected override void OnDispose()
+    {
+        base.OnDispose();
+        _currentTracker = null;
+    }
+}
