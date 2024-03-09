b05312648616177a9c7f80f8fc12e1ba21b5c895
xLinka <Oliver.dunnington@outlook.com>
2024-03-09 18:51:35 +0000

    start

---
+++ ProtoFlux/Devices/OpenVR/IndexOfRole.cs
@@ -0,0 +1,20 @@
+﻿using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using Valve.VR;
+
+namespace OpenvrDataGetter.ProtoFlux
+{
+    [NodeName("IndexOfRole")]
+    [Category("ProtoFlux/OpenVR/DataGetter")]
+    public class IndexOfRole : ValueFunctionNode<FrooxEngineContext, uint>
+    {
+        public readonly ValueInput<ETrackedControllerRole> Role;
+        protected override uint Compute(FrooxEngineContext context)
+        {
+            var role = Role.Evaluate(context);
+            return OpenVR.System.GetTrackedDeviceIndexForControllerRole(role);
+        }
+    }
+}
---
+++ ProtoFlux/Devices/OpenVR/TrackedDeviceIndex.cs
@@ -0,0 +1,19 @@
+﻿using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace OpenvrDataGetter.ProtoFlux
+{
+    [NodeName("TrackedDeviceIndex")]
+    [Category("ProtoFlux/OpenVR")]
+    public class TrackedDeviceIndex : ValueFunctionNode<FrooxEngineContext, uint>
+    {
+        public readonly ValueInput<uint> Index;
+
+        protected override uint Compute(FrooxEngineContext context)
+        {
+            return Index.Evaluate(context); 
+        }
+    }
+}
