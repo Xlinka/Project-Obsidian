2c2fb29503aa8f0e287f304d8ef33e1ff33a6724
LeCloutPanda <53411604+LeCloutPanda@users.noreply.github.com>
2024-04-04 08:11:41 +1100

    Create IsUserInSeatedMode.cs
    Hand over the slushie

---
+++ Bindings/User/IsUserInSeatedMode.cs
@@ -0,0 +1,19 @@
+﻿using FrooxEngine;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace FrooxEngine.ProtoFlux.Locomotion
+{
+    [ContinuouslyChanging]
+    [NodeCategory("ProtoFlux/Obsidian/Users/Status")]
+    public class IsUserInSeatedModeNode : ValueFunctionNode<ExecutionContext, bool>
+    {
+        public readonly ObjectInput<User> User;
+
+        protected override bool Compute(ExecutionContext context)
+        {
+            User user = User.Evaluate(context);
+            return user == null ? false : user.InputInterface.SeatedMode;
+        }
+    }
+}
