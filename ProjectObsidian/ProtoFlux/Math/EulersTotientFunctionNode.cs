047d1f1fb98835fb7c738e84457428b806a16edf
xLinka <Oliver.dunnington@outlook.com>
2024-06-25 14:29:53 +0100

    Eulerstoient node and IsPrimeNumber

---
+++ ProjectObsidian/ProtoFlux/Math/EulersTotientFunctionNode.cs
@@ -0,0 +1,28 @@
+﻿using Elements.Core;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math
+{
+    [NodeCategory("Obsidian/Math")]
+    [NodeName("EulersTotientFunction")]
+    public class EulersTotientFunctionNode : ValueFunctionNode<FrooxEngineContext, int>
+    {
+        public ValueInput<int> Input;
+
+        protected override int Compute(FrooxEngineContext context)
+        {
+            var result = Input.Evaluate(context);
+            var inputCopy = result;
+            for (var p = 2; p * p <= inputCopy; ++p)
+            {
+                if (inputCopy % p != 0) continue;
+                while (inputCopy % p == 0) inputCopy /= p;
+                result -= result / p;
+            }
+            if (inputCopy > 1) result -= result / inputCopy;
+            return result;
+        }
+    }
+}
---
+++ ProjectObsidian/ProtoFlux/Math/IsPrimeNumberNode.cs
@@ -0,0 +1,40 @@
+﻿using Elements.Core;
+using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math
+{
+    [NodeCategory("Obsidian/Math")]
+    [NodeName("IsPrimeNumber")]
+    public class IsPrimeNumberNode : ValueFunctionNode<FrooxEngineContext, bool>
+    {
+        public ValueInput<int> Input;
+
+        protected override bool Compute(FrooxEngineContext context)
+        {
+            var num = Input.Evaluate(context);
+            switch (num)
+            {
+                case < 2:
+                    return false;
+                case 2:
+                    return true;
+                default:
+                    {
+                        if (num % 2 == 0) return false;
+                        break;
+                    }
+            }
+            double sqrtNum = MathX.Sqrt(num);
+            for (var i = 3; i <= sqrtNum; i += 2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
