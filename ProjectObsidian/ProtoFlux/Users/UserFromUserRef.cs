70992daa0bed8b2d0d7cb149498dc28c1045e2a9
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-06-27 15:41:22 +0100

    code uploads
    but for some reason theyre not uploading

---
+++ ProjectObsidian/ProtoFlux/Users/UserFromUserRef.cs
@@ -0,0 +1,20 @@
+﻿using FrooxEngine.ProtoFlux;
+using ProtoFlux.Core;
+using FrooxEngine;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Users
+{
+    [ContinuouslyChanging]
+    [NodeCategory("Obsidian/Users")]
+    [NodeName("UserFromUserRef")]
+    public class UserFromUserRef : ObjectFunctionNode<FrooxEngineContext, User>
+    {
+        public readonly ObjectInput<UserRef> UserRef;
+
+        protected override User Compute(FrooxEngineContext context)
+        {
+            UserRef userRef = UserRef.Evaluate(context);
+            return userRef.Target;
+        }
+    }
+}
