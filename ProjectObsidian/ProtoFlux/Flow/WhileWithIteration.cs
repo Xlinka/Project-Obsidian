ff19e617d2b77fd097750b95ca3ee80aca8457ba
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-06-27 20:34:29 +0100

    While nodes with iteration

---
+++ ProjectObsidian/ProtoFlux/Flow/WhileWithIteration.cs
@@ -0,0 +1,39 @@
+﻿using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Flow
+{
+    [NodeCategory("Obsidian/Flow")]
+    [NodeName("While With I", false)]
+    public class WhileWithIteration : ActionNode<ExecutionContext>
+    {
+        public ValueInput<bool> Condition;
+        public Call LoopStart;
+        public Call LoopIteration;
+        public Call LoopEnd;
+        public readonly ValueOutput<int> i;
+        private int iter;
+
+        protected override IOperation Run(ExecutionContext context)
+        {
+            iter = 0;
+            LoopStart.Execute(context);
+            while (Condition.Evaluate(context, defaultValue: false))
+            {
+                iter++;
+                i.Write(iter, context);
+                if (context.AbortExecution)
+                {
+                    throw new ExecutionAbortedException(base.Runtime as IExecutionRuntime, this, LoopIteration.Target, isAsync: false);
+                }
+                LoopIteration.Execute(context);
+            }
+            return LoopEnd.Target;
+        }
+
+        public WhileWithIteration()
+        {
+            i = new ValueOutput<int>(this);
+        }
+    }
+}
