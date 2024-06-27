d20960998ae8f2273c77a5a3af04ba8e52058cff
Nytra <14206961+Nytra@users.noreply.github.com>
2024-06-27 05:07:56 +0100

    Add ComponentsDataFeed and ComponentDataItemInterface

---
+++ ProjectObsidian/Components/Radiant UI/Data Feeds/Interfaces/ComponentDataItemInterface.cs
@@ -0,0 +1,24 @@
+﻿using FrooxEngine;
+
+namespace Obsidian;
+
+[Category(new string[] { "Obsidian/Radiant UI/Data Feeds/Interfaces" })]
+public class ComponentDataItemInterface : FeedItemInterface
+{
+    public readonly SyncRef<SyncRef<Component>> Component;
+
+    public readonly SyncRef<IField<int>> MemberCount;
+
+    public readonly FeedSubTemplate<DataFeedEntity<ISyncMember>, FeedEntityInterface<ISyncMember>> Members;
+
+    public override void Set(IDataFeedView view, DataFeedItem item)
+    {
+        base.Set(view, item);
+        if (item is ComponentDataFeedItem componentDataFeedItem)
+        {
+            Component.TrySetTarget(componentDataFeedItem.Data.component);
+            MemberCount.TrySetTarget(componentDataFeedItem.Data.MemberCount);
+            Members.Set(componentDataFeedItem.Members, view);
+        }
+    }
+}
