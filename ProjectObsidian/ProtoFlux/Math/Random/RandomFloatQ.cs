efe780833564412c81fe911f9ad133360a50d649
xLinka <Oliver.dunnington@outlook.com>
2024-07-20 23:56:42 +0100

    Added Node based on https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/2642
    https://github.com/Yellow-Dog-Man/Resonite-Issues/issues/2642

---
+++ ProjectObsidian/ProtoFlux/Math/Random/RandomFloatQ.cs
@@ -0,0 +1,25 @@
+﻿using Elements.Core;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using Obsidian;
+
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Math.Random
+{
+
+    [NodeCategory("Obsidian/Math/Random")]
+    [ContinuouslyChanging]
+    public class RandomFloatQ : ValueFunctionNode<ExecutionContext, floatQ>
+    {
+        public readonly ValueInput<floatQ> Min;
+        public readonly ValueInput<floatQ> Max;
+
+        protected override floatQ Compute(ExecutionContext context)
+        {
+            floatQ min = Min.Evaluate(context);
+            floatQ max = Max.Evaluate(context);
+
+            return RandomXExtensions.Range(min, max);
+        }
+    }
+}
