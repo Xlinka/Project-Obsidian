66881e2ee57ece09d209ead30d327d20897c50ea
Nytra <14206961+Nytra@users.noreply.github.com>
2025-01-05 17:00:39 +0000

    Add AudioSourceInfo

---
+++ ProjectObsidian/ProtoFlux/Audio/AudioSourceInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ProtoFlux.Core;
+using ProtoFlux.Runtimes.Execution;
+using FrooxEngine.ProtoFlux;
+using FrooxEngine;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Audio
+{
+    [NodeCategory("Obsidian/Audio")]
+    public class AudioSourceInfo : VoidNode<FrooxEngineContext>
+    {
+        public readonly ObjectInput<IAudioSource> Source;
+
+        [ContinuouslyChanging]
+        public readonly ValueOutput<bool> IsActive;
+
+        public readonly ValueOutput<int> ChannelCount;
+
+        protected override void ComputeOutputs(FrooxEngineContext context)
+        {
+            IAudioSource source = Source.Evaluate(context);
+            if (source != null)
+            {
+                IsActive.Write(source.IsActive, context);
+                ChannelCount.Write(source.ChannelCount, context);
+            }
+            else
+            {
+                IsActive.Write(false, context);
+                ChannelCount.Write(0, context);
+            }
+        }
+
+        public AudioSourceInfo()
+        {
+            IsActive = new ValueOutput<bool>(this);
+            ChannelCount = new ValueOutput<int>(this);
+        }
+    }
+}
