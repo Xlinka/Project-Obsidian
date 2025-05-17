996af7ed58f054b602ba87c63fb2380d113abe9f
Nytra <14206961+Nytra@users.noreply.github.com>
2025-05-17 15:07:25 +0100

    Json select nodes

---
+++ ProjectObsidian/ProtoFlux/JSON/JsonSelectTokenNode.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using FrooxEngine;
+using FrooxEngine.ProtoFlux;
+using Obsidian.Elements;
+using ProtoFlux.Runtimes.Execution;
+using ProtoFlux.Core;
+using Newtonsoft.Json.Linq;
+using System;
+using Elements.Core;
+
+namespace ProtoFlux.Runtimes.Execution.Nodes.Obsidian.Json;
+
+[NodeName("Select Token")]
+[NodeCategory("Obsidian/Json")]
+public class JsonSelectTokenNode : ObjectFunctionNode<FrooxEngineContext, string>
+{
+    public readonly ObjectInput<JsonObject> JsonObject;
+    public readonly ObjectInput<string> Path;
+    protected override string Compute(FrooxEngineContext context)
+    {
+        var json = JsonObject.Evaluate(context);
+        var path = Path.Evaluate(context);
+
+        if (json is null || string.IsNullOrWhiteSpace(path)) return null;
+
+        try
+        {
+            JToken thing = json.Wrapped.SelectToken(path);
+            return thing.ToString();
+        }
+        catch (Exception ex)
+        {
+            UniLog.Error($"Error in json path node: {ex}");
+            return null;
+        }
+    }
+}
