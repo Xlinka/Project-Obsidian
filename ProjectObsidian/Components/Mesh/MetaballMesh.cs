95dca9840ae511f112f85f598c6d38e84cd56e8f
Nytra <14206961+Nytra@users.noreply.github.com>
2025-05-31 11:28:12 +0100

    Metaballs working

--- ProjectObsidian/Components/Mesh/MetaballMesh.cs
+++ ProjectObsidian/Components/Mesh/MetaballMesh.cs
@@ -1,7 +1,6 @@
 ﻿using Elements.Assets;
 using Elements.Core;
 using FrooxEngine;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,32 +9,87 @@ namespace Obsidian
     [Category(new string[] { "Obsidian/Assets/Procedural Meshes" })]
     public class MetaballMesh : ProceduralMesh
     {
+        public readonly Sync<float> Threshold;
+        public readonly Sync<float3> FieldSize;
+        public readonly Sync<int> Resolution;
         public readonly SyncRefList<MetaballPoint> Points;
         private MetaballShape shape;
         private List<MetaballPoint> _points;
-        protected override void OnAwake()
+        private float _threshold;
+        private float3 _fieldSize;
+        private int _resolution;
+        private HashSet<MetaballPoint> _subscribedPoints = new();
+        private bool dirty;
+        protected override void OnStart()
         {
+            Points.Changed += OnListChange;
+            foreach (var point in Points)
+            {
+                point.Slot.WorldTransformChanged += OnWorldTransformChanged;
+                point.Changed += OnChange;
+                _subscribedPoints.Add(point);
+            }
+        }
+
+        private void OnListChange(IChangeable change)
+        {
+            foreach (var point in _subscribedPoints)
+            {
+                point.Slot.WorldTransformChanged -= OnWorldTransformChanged;
+                point.Changed -= OnChange;
+            }
+            _subscribedPoints.Clear();
+            foreach (var point in Points)
+            {
+                if (point != null)
+                {
+                    point.Slot.WorldTransformChanged += OnWorldTransformChanged;
+                    point.Changed += OnChange;
+                    _subscribedPoints.Add(point);
+                }
+            }
+        }
+
+        private void OnChange(IChangeable change)
+        {
+            MarkChangeDirty();
+            dirty = true;
+        }
+
+        private void OnWorldTransformChanged(Slot s)
+        {
+            MarkChangeDirty();
+            dirty = true;
         }
 
         protected override void PrepareAssetUpdateData()
         {
             _points = Points.ToList();
+            _threshold = Threshold.Value;
+            _fieldSize = FieldSize.Value;
+            _resolution = Resolution.Value;
         }
 
         protected override void ClearMeshData()
         {
+            shape?.Remove();
             shape = null;
         }
 
         protected override void UpdateMeshData(MeshX meshx)
         {
-            if (shape == null || shape.Points != _points)
+            if (shape == null || dirty)
             {
                 shape?.Remove();
-                shape = new MetaballShape(meshx, _points);
+                shape = new MetaballShape(meshx, _points, Slot);
             }
 
+            shape.Threshold = _threshold;
+            shape.FieldSize = _fieldSize;
+            shape.Resolution = _resolution;
+            meshx.Clear();
             shape.Update();
+            dirty = false;
         }
     }
 }
--- ProjectObsidian/Components/Mesh/MetaballPoint.cs
+++ ProjectObsidian/Components/Mesh/MetaballPoint.cs
@@ -1,13 +1,19 @@
-﻿using Elements.Assets;
-using Elements.Core;
+﻿using Elements.Core;
 using FrooxEngine;
-using System;
 
 namespace Obsidian
 {
     [Category(new string[] { "Obsidian/Assets/Procedural Meshes" })]
     public class MetaballPoint : Component
     {
-        // owo
+        public readonly Sync<float> Radius;
+        public readonly Sync<float> Strength;
+
+        public float GetValue(float3 point, Slot space = null)
+        {
+            float distance = MathX.Distance(point, (space ?? World.RootSlot).GlobalPointToLocal(Slot.GlobalPosition));
+            if (distance == 0) return float.MaxValue;
+            return Strength * (Radius * Radius) / (distance * distance);
+        }
     }
 }
