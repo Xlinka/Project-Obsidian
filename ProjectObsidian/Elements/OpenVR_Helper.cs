2b020887be2cba0eb91935dfff3eeb515b546c30
Nytra <14206961+Nytra@users.noreply.github.com>
2025-08-16 07:53:19 +0100

    Better OpenVR error handling

--- ProjectObsidian/Elements/OpenVR_Helper.cs
+++ ProjectObsidian/Elements/OpenVR_Helper.cs
@@ -11,25 +11,26 @@ namespace Obsidian;
 public static class OpenVR_Helper
 {
     private static bool _initialized = false;
-    private static bool _steamVrNotRunning = false;
+    private static bool _unableToConnect = false;
     public static CVRSystem VRSystem {get; private set;}
     public static bool TryInitialize()
     {
         if (_initialized) return true;
-        if (_steamVrNotRunning) return false;
+        if (_unableToConnect) return false;
         EVRInitError error = EVRInitError.None;
         VRSystem = OpenVR.Init(ref error, eApplicationType: EVRApplicationType.VRApplication_Background);
-        if (error == EVRInitError.Init_NoServerForBackgroundApp)
-        {
-            UniLog.Log("SteamVR is not running");
-            _steamVrNotRunning = true;
-            return false;
-        }
-        else
+        if (error == EVRInitError.None)
         {
             UniLog.Log("Got SteamVR connection");
             _initialized = true;
             return true;
+        }
+        else
+        {
+            // EVRInitError.Init_NoServerForBackgroundApp means SteamVR is not running
+            UniLog.Error($"Error when initializing OpenVR: {error.ToString()}");
+            _unableToConnect = true;
+            return false;
         }
     }
 }
